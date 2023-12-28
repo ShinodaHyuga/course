d2d937ab153eb5dbae8eadb6de5c5990f7676398
篠田彪河 <84775200+ShinodaHyuga@users.noreply.github.com>
2023-12-28 12:23:40 +0900

    Update ToDoApp.java(データベースの初期化コードを追加: line 53, 54)

--- ToDoApp.java
+++ ToDoApp.java
@@ -4,12 +4,15 @@ import android.content.ContentValues;
 import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.os.Bundle;
+import android.text.TextUtils;
 import android.view.View;
+import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.LinearLayout;
 import android.widget.ListView;
+import android.widget.Toast;
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ExSample extends AppCompatActivity {
@@ -18,6 +21,7 @@ public class ExSample extends AppCompatActivity {
     private Button addTodoButton, showTodosButton, deleteTodoButton;
     private ListView todoListView;
     private SQLiteDatabase db;
+    private ArrayAdapter<String> todoAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -38,13 +42,21 @@ public class ExSample extends AppCompatActivity {
         showTodosButton = new Button(this);
         showTodosButton.setText("Show Todos");
         deleteTodoButton = new Button(this);
-        deleteTodoButton.setText("Delete ToDo");
+        deleteTodoButton.setText("Delete Selected ToDo");
         todoListView = new ListView(this);
 
         // データベースの初期化
         String dbPath = "data/data/" + getPackageName() + "/ToDoApp.db";
         db = SQLiteDatabase.openOrCreateDatabase(dbPath, null);
         createTables();
+
+        // 既存のデータベースを削除する場合
+        // this.deleteDatabase(dbPath);
+
+        // ToDoアイテムのリスト用のアダプターを初期化
+        todoAdapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_single_choice);
+        todoListView.setAdapter(todoAdapter);
+        todoListView.setChoiceMode(ListView.CHOICE_MODE_SINGLE);
 
         // レイアウトにUIコンポーネントを追加
         ll.addView(usernameEditText);
@@ -72,7 +84,17 @@ public class ExSample extends AppCompatActivity {
         deleteTodoButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                deleteTodoItem();
+                deleteSelectedTodoItem();
+            }
+        });
+
+        // リストアイテムが選択されたときのリスナーを設定
+        todoListView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> adapterView, View view, int position, long id) {
+                // 選択したアイテムをEditTextに表示
+                String selectedTodo = todoAdapter.getItem(position);
+                todoEditText.setText(selectedTodo);
             }
         });
     }
@@ -88,6 +110,12 @@ public class ExSample extends AppCompatActivity {
     private void addTodoItem() {
         String username = usernameEditText.getText().toString();
         String todoItem = todoEditText.getText().toString();
+
+        // 入力が空白でないことを確認
+        if (TextUtils.isEmpty(todoItem.trim())) {
+            Toast.makeText(this, "ToDo item cannot be empty or whitespace.", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
         // ユーザが存在しなければ新規登録
         long userId = getUserId(username);
@@ -123,9 +151,14 @@ public class ExSample extends AppCompatActivity {
         return db.insert("users", null, values);
     }
 
-    private void deleteTodoItem() {
+    private void deleteSelectedTodoItem() {
         String username = usernameEditText.getText().toString();
-        String todoItem = todoEditText.getText().toString();
+        String selectedTodo = todoEditText.getText().toString();
+
+        if (selectedTodo.isEmpty()) {
+            Toast.makeText(this, "Please select a ToDo item to delete.", Toast.LENGTH_SHORT).show();
+            return;
+        }
 
         // ユーザが存在しなければ何もしない
         long userId = getUserId(username);
@@ -134,13 +167,12 @@ public class ExSample extends AppCompatActivity {
         }
 
         // ToDoアイテムの削除
-        db.delete("todos", "user_id = ? AND todo = ?", new String[]{String.valueOf(userId), todoItem});
+        db.delete("todos", "user_id = ? AND todo = ?", new String[]{String.valueOf(userId), selectedTodo});
 
         // 削除後の表示を更新
         showTodoItems();
 
         // 入力フィールドのクリア
-        usernameEditText.setText("");
         todoEditText.setText("");
     }
 
@@ -157,12 +189,19 @@ public class ExSample extends AppCompatActivity {
         Cursor cursor = db.rawQuery("SELECT todo FROM todos WHERE user_id = ?", new String[]{String.valueOf(userId)});
 
         // 結果をListViewに表示
-        ArrayAdapter<String> adapter = new ArrayAdapter<>(this, android.R.layout.simple_list_item_1);
+        todoAdapter.clear();
         while (cursor.moveToNext()) {
             String todoItem = cursor.getString(cursor.getColumnIndex("todo"));
-            adapter.add(todoItem);
+            todoAdapter.add(todoItem);
         }
 
-        todoListView.setAdapter(adapter);
+        // ToDoアイテムが存在しない場合はEditTextとDeleteボタンを非表示にする
+        if (todoAdapter.getCount() == 0) {
+            todoEditText.setVisibility(View.GONE);
+            deleteTodoButton.setVisibility(View.GONE);
+        } else {
+            todoEditText.setVisibility(View.VISIBLE);
+            deleteTodoButton.setVisibility(View.VISIBLE);
+        }
     }
 }
